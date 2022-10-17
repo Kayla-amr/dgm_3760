9fa2eae1de0b531fd360648871fba83344b21ec1
Kayla Macias <kaylamacias.rivera@gmail.com>
2022-10-17 10:49:56 -0600

    adding css

---
+++ .history/todoTemplate-W2/index_20221017104840.js
@@ -0,0 +1,40 @@
+let myList = [
+  {
+    id: 0,
+    name: "Choose photography theme",
+    category: "Homework",
+    due_date: "Tomorrow",
+    status: false,
+  },
+  {
+    id: 1,
+    name: "Organize music",
+    category: "Chores",
+    due_date: "Wednesday",
+    status: false,
+  },
+];
+
+//app title
+document.querySelector(".formTitle").innerText = "Basic App Form";
+
+
+
+//view todods
+myList.forEach(function(myTask){
+  document.querySelector('.tasks').innerHTML += `<li> ${myTask.name} </li>`;
+});
+
+
+//add todos
+function addTask(){
+  myList = myList.map(function(newTask){
+    newTask = []
+  })
+}
+
+// myTask(myList, (num) => {
+//   console.log (num)
+// })
+
+
